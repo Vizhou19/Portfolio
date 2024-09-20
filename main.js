9064b954ae765b155012e02cd3268260c5011d11
Rene Cardenas <renecar616@gmail.com>
2024-09-20 16:53:11 -0500

    added Imgs and Websites to this.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+let projectName = document.getElementsByClassName("projects-desc");
+let projectImg = document.getElementsByClassName("project-img");
 
+projectImg.addEventListener("mouseover", function () {
+  projectName.style.display = "block";
+});
+
+projectImg.addEventListener("mouseout", function () {
+  projectName.style.display = "none";
+});
