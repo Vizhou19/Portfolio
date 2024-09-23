51219592cb98be2fbaea523f7e09c0bd72972894
Rene Cardenas <renecar616@gmail.com>
2024-09-23 13:09:37 -0500

    Added Some JS Functions and fixed some minor bugs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let projectName = document.getElementsByClassName("projects-desc");
+/* let projectName = document.getElementsByClassName("projects-desc");
 let projectImg = document.getElementsByClassName("project-img");
 
 projectImg.addEventListener("mouseover", function () {
@@ -7,4 +7,28 @@ projectImg.addEventListener("mouseover", function () {
 
 projectImg.addEventListener("mouseout", function () {
   projectName.style.display = "none";
+}); */
+
+// * Cozy Cafe
+let cafeImg = document.getElementById("cafe-img");
+let cafeName = document.getElementById("cafe-name");
+
+cafeImg.addEventListener("mouseover", function () {
+  cafeName.style.display = "block";
+});
+
+cafeImg.addEventListener("mouseout", function () {
+  cafeName.style.display = "none";
+});
+
+//* Gaming Calculator
+let calcImg = document.getElementById("calc-img");
+let calcName = document.getElementById("calc-name");
+
+calcImg.addEventListener("mouseover", function () {
+  calcName.style.display = "block";
+});
+
+calcImg.addEventListener("mouseout", function () {
+  calcName.style.display = "none";
 });
